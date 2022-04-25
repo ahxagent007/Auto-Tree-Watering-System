502662756611cc503c4a9e6dd193cd196b423d94
De Xian <ahx.agent007@gmail.com>
2022-04-26 04:24:42 +0600

    Working on weather data

--- app/src/main/java/com/alphacuetech/xian/autotree/Menu.java
+++ app/src/main/java/com/alphacuetech/xian/autotree/Menu.java
@@ -2,17 +2,62 @@ package com.alphacuetech.xian.autotree;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 
 public class Menu extends AppCompatActivity {
 
-    String API_LINK = "https://api.openweathermap.org/data/2.5/onecall?lat=23.797214&lon=90.365007&exclude=current&appid=c33cae6efe8bce29a5eba0f84d6bad4c";
+    String TAG = "XIAN";
+
+    String API_LINK_TEST = "https://api.openweathermap.org/data/2.5/onecall?lat=23.797214&lon=90.365007&exclude=current&appid=c33cae6efe8bce29a5eba0f84d6bad4c";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        getWeatherData("23.797214", "90.365007", getApplicationContext());
+    }
+
+    private RequestQueue mRequestQueue;
+    private StringRequest mStringRequest;
+
+    public void getWeatherData(String lat, String lon, Context contex){
+
+        String url = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=current&appid=c33cae6efe8bce29a5eba0f84d6bad4c";
+
+        mRequestQueue  = Volley.newRequestQueue(contex);
+
+        //String Request initialized
+        mStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                String responseText = response.toString();
+                Toast.makeText(contex,"Response :" + responseText, Toast.LENGTH_LONG).show();//display the response on screen
+                Log.i(TAG, responseText);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Log.i(TAG,"Error :" + error.toString());
+            }
+        });
+
+        mRequestQueue.add(mStringRequest);
+
+
     }
 }
---
+++ app/src/main/java/com/alphacuetech/xian/autotree/functions/API_CALLS.java
@@ -0,0 +1,19 @@
+package com.alphacuetech.xian.autotree.functions;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.android.volley.Response;
+
+public class API_CALLS {
+
+
+
+
+}
