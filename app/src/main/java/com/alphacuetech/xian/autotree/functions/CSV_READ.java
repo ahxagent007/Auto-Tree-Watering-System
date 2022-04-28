638bae26cbd76fe9fd646d0dae34f90ec93c7cc8
De Xian <ahx.agent007@gmail.com>
2022-04-28 23:15:35 +0600

    F:\Secret Dev\Peoples\Batuli\Thesis

---
+++ app/src/main/java/com/alphacuetech/xian/autotree/functions/CSV_READ.java
@@ -0,0 +1,25 @@
+package com.alphacuetech.xian.autotree.functions;
+/*
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.IOException;
+import java.io.FileReader;
+
+
+public class CSV_READ {
+
+    public void getCsvData(){
+        try {
+            CSVReader reader = new CSVReader(new FileReader("data.csv"));
+            String[] nextLine;
+            while ((nextLine = reader.readNext()) != null) {
+                // nextLine[] is an array of values from the line
+                System.out.println(nextLine[0] + nextLine[1] + "etc...");
+            }
+        } catch (IOException | CsvValidationException e) {
+
+        }
+    }
+
+}*/
